7c2957d3c7ce325da8ad9908c0e23dd128b75257
warrior14 <ironaddicts14@gmail.com>
2021-09-02 18:52:32 -0500

    added data to each file

---
+++ Ram.cs
@@ -0,0 +1,14 @@
+namespace Garage
+{
+    public class Ram  // Gas powered truck
+    {
+        public double FuelCapacity { get; set; }
+        public string MainColor { get; set; }
+        public int MaximumOccupancy { get; set; }
+
+        public void RefuelTank()
+        {
+            // method definition omitted
+        }
+    }
+}
