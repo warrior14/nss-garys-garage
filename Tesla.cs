0b36be90c08d82cb1fb00877c0690462744210bd
warrior14 <ironaddicts14@gmail.com>
2021-09-02 22:39:14 -0500

    finished the garys exercise

--- Tesla.cs
+++ Tesla.cs
@@ -2,13 +2,31 @@ using System;
 
 namespace Garage
 {
-    public class Tesla : Vehicle
+    public class Tesla : Vehicle // you need a colon between the subclass name and the base/parent class name when inheriting 
+    // now with any instance of Tesla will have the same properties and methods from the base/parent class
     {
         public double BatteryKWh { get; set; }
 
         public void ChargeBattery()
         {
             // method definition omitted
+            Console.WriteLine($"Your car is now charged up to {BatteryKWh} KWh");
+            Console.WriteLine();
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"The {MainColor} Tesla with {MaximumOccupancy} passengers zips by you.");
+        }
+
+        public override void Turn(string direction)
+        {
+            Console.WriteLine($"The {MainColor} Tesla zooms around a {direction} turn!");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine($"The {MainColor} Tesla abrupty stops.");
         }
     }
 }
--- Zero.cs
+++ Zero.cs
@@ -10,6 +10,23 @@ namespace Garage
         public void ChargeBattery()
         {
             // method definition omitted
+            Console.WriteLine($"Your car is now charged to {BatteryKWh} KWh.");
+            Console.WriteLine();
+        }
+
+        public override void Drive()
+        {
+            Console.WriteLine($"The {MainColor} Zero with {MaximumOccupancy} passengers!");
+        }
+
+        public override void Turn(string direction)
+        {
+            Console.WriteLine($"The {MainColor} Zero carefully turns {direction}.");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine($"The {MainColor} Zero stops!");
         }
     }
 }
